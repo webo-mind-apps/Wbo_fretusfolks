67d8f421996287bf93b76b605f09fcc06c223af1
shahirWebo <shahir.webomindapps@gmail.com>
2020-01-25 10:33:19 +0530

    unwanted files deleted

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'https://app.fretusfolks.com/';
-$config['base_url'] = 'https://localhost/Wbo_fretusfolks/';
+$config['base_url'] = '//localhost/Wbo_fretusfolks/';
 
 /*
 |--------------------------------------------------------------------------
