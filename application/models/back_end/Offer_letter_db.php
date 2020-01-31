7781d5b314dc9caa14ece6fd21e4f9ff7650b720
Madhusudhan <madhusudhan.webomindapps.com>
2020-01-31 10:18:36 +0530

    offer_letter_multiple_pdf_generation_madhu

--- application/models/back_end/Offer_letter_db.php
+++ application/models/back_end/Offer_letter_db.php
@@ -126,6 +126,18 @@ class Offer_letter_db extends CI_Model
 		
 		
 	}
+	function get_offer_letter_pdf()
+	{
+		// $id=$this->uri->segment(3);
+		$this->db->select('a.*,b.emp_name,b.ffi_emp_id,b.joining_date,b.branch,b.location,b.designation,b.department,b.father_name,b.contract_date,c.client_name');
+		$this->db->from('offer_letter a');
+		$this->db->join('backend_management b','a.employee_id=b.ffi_emp_id','left');
+		$this->db->join('client_management c','a.company_id=c.id','left');
+		// $this->db->where('a.id',$id);
+		$query=$this->db->get();
+		$q=$query->result_array();
+		return $q;
+	}
 	function get_all_states()
 	{
 		$this->db->order_by('state_name','ASC');
@@ -167,5 +179,4 @@ class Offer_letter_db extends CI_Model
 
 		
 	}
-}  
-?>
+}
