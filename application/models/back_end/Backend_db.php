684681d452d33aa584d0dee06719ff4d88e8371a
Madhusudhan <madhusudhan.webomindapps.com>
2020-02-05 11:54:01 +0530

    datatables_fundamentals_ok by madhu

--- application/models/back_end/Backend_db.php
+++ application/models/back_end/Backend_db.php
@@ -21,9 +21,66 @@ class Backend_db extends CI_Model
 		$this->db->where('a.active_status',0); 
 		$this->db->order_by('a.id','DESC');
 		$query=$this->db->get();
-		$q=$query->result_array();
-		return $q;
+		// $q=$query->result_array();
+	
+		return $query;
 	}
+
+	public function make_query()
+	{
+	 
+        $order_column = array("a.id", "client_name", "ffi_emp_id", "emp_name", "joining_date", "phone1","data_status");  
+        $this->db->select('a.*,b.client_name,c.state_name');
+		$this->db->from('backend_management a');
+		$this->db->join('client_management b','a.client_id=b.id','left');
+		$this->db->join('states c','a.state=c.id','left');
+		$this->db->where('emp_name!=','');
+		$this->db->where("a.status","0");
+		$this->db->where("a.dcs_approval","1");
+		$this->db->where('a.active_status',0);
+		if(isset($_POST["search"]["value"])){
+            $this->db->group_start();
+                $this->db->like("a.id", $_POST["search"]["value"]);  
+                $this->db->or_like("client_name", $_POST["search"]["value"]);  
+                $this->db->or_like("ffi_emp_id", $_POST["search"]["value"]);  
+                $this->db->or_like("emp_name", $_POST["search"]["value"]);
+				$this->db->or_like("joining_date", $_POST["search"]["value"]);
+				$this->db->or_like("phone1", $_POST["search"]["value"]); 
+                $this->db->or_like("data_status", $_POST["search"]["value"]); 
+            $this->db->group_end();
+		}
+		if(isset($_POST["order"]))  
+        {  
+             $this->db->order_by($order_column[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);  
+        }  
+        else  
+        {  
+             $this->db->order_by('a.id', 'DESC');  
+        }  	
+	}
+
+	function get_all_data()  
+    {  
+           $this->db->select("*");
+           $this->db->from('backend_management');  
+           return $this->db->count_all_results();  
+	}
+	
+	function get_filtered_data(){  
+		$this->make_query();  
+		$query = $this->db->get();  
+		return $query->num_rows();  
+	} 
+
+	function make_datatables(){  
+        $this->make_query();   
+		if($_POST["length"] != -1)  
+		{  
+			 $this->db->limit($_POST['length'], $_POST['start']);  
+		}  
+		$query = $this->db->get();  
+		return $query->result();  
+    }
 	
 	function get_backend_team_details($id)
 	{
@@ -900,5 +957,4 @@ class Backend_db extends CI_Model
 	}
 
 	
-}  
-?>
+}
