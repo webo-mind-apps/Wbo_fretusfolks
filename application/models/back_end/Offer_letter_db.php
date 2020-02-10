443df8baa19162dd047ca51cb24c793096aac45e
Madhusudhan <madhusudhan.webomindapps.com>
2020-02-10 12:23:22 +0530

    dtable offfer change again

--- application/models/back_end/Offer_letter_db.php
+++ application/models/back_end/Offer_letter_db.php
@@ -104,7 +104,6 @@ class Offer_letter_db extends CI_Model
 		if ($tenure_date != "") {
 			$tenure_date_db = date("Y-m-d", strtotime($tenure_date));
 		}
-<<<<<<< HEAD
 
 		$basic_salary = $this->input->post('basic_salary');
 		$hra = $this->input->post('hra');
@@ -149,45 +148,6 @@ class Offer_letter_db extends CI_Model
 		$query = $this->db->get();
 		$q = $query->result_array();
 		return $q;
-=======
-		
-		$basic_salary=$this->input->post('basic_salary');
-		$hra=$this->input->post('hra');
-		$conveyance=$this->input->post('conveyance');
-		$medical=$this->input->post('medical');
-		$special_allowance=$this->input->post('special_allowance');
-		$other_allowance=$this->input->post('other_allowance');
-		$gross_salary=$this->input->post('gross_salary');
-		
-		$emp_pf=$this->input->post('emp_pf');
-		$emp_esic=$this->input->post('emp_esic');
-		$pt=$this->input->post('pt');
-		$total_deduction=$this->input->post('total_deduction');
-		$take_home=$this->input->post('take_home');
-		$employer_pf=$this->input->post('employer_pf');
-		$employer_esic=$this->input->post('employer_esic');
-		$mediclaim=$this->input->post('mediclaim');
-		$ctc=$this->input->post('ctc');
-		
-		$date=date("Y-m-d");
-		$data=array("company_id"=>$client,"employee_id"=>$emp_id,"date"=>$date,"offer_letter_type"=>$letter_format,"basic_salary"=>$basic_salary,"hra"=>$hra,"conveyance"=>$conveyance,"medical_reimbursement"=>$medical,"special_allowance"=>$special_allowance,"other_allowance"=>$other_allowance,"gross_salary"=>$gross_salary,"emp_pf"=>$emp_pf,"emp_esic"=>$emp_esic,"pt"=>$pt,"total_deduction"=>$total_deduction,"take_home"=>$take_home,"employer_pf"=>$employer_pf,"employer_esic"=>$employer_esic,"mediclaim"=>$mediclaim,"ctc"=>$ctc);
-		$this->db->where('employee_id',$emp_id);
-		$this->db->where('company_id',$client);
-		$query=$this->db->get("offer_letter");
-		if(!$query->num_rows())
-		{
-			$this->db->insert('offer_letter',$data);
-			$this->db->select('a.*,b.branch,b.emp_name,b.last_name,b.middle_name,b.ffi_emp_id,b.email,b.joining_date,b.location,b.designation,b.department,b.father_name,b.contract_date,c.client_name');
-			$this->db->from('offer_letter a');
-			$this->db->join('backend_management b','a.employee_id=b.ffi_emp_id','left');
-			$this->db->join('client_management c','a.company_id=c.id','left');
-			$this->db->where('a.employee_id',$emp_id);
-			$query=$this->db->get();
-			$q=$query->result_array();
-			return $q;
-		}
-		
->>>>>>> 6a51a23a99260cebddf4e671fde11562b7de8195
 	}
 	function get_offer_letter()
 	{
@@ -225,18 +185,11 @@ class Offer_letter_db extends CI_Model
 		$q = $query->result_array();
 		return $q;
 	}
-<<<<<<< HEAD
 	function get_all_states()
 	{
 		$this->db->order_by('state_name', 'ASC');
 		$query = $this->db->get("states");
 		$q = $query->result_array();
-=======
-	function get_all_states(){
-		$this->db->order_by('state_name','ASC');
-		$query=$this->db->get("states");
-		$q=$query->result_array();
->>>>>>> 6a51a23a99260cebddf4e671fde11562b7de8195
 		return $q;
 	}
 	function get_all_clients()
@@ -257,31 +210,7 @@ class Offer_letter_db extends CI_Model
 	// excel import
 	public function importEmployee_offer_letter($data = null)	{
 
-<<<<<<< HEAD
 		$this->db->where('employee_id', $data['employee_id']);
-=======
-		$this->db->where('employee_id',$data['employee_id']);
-		
-		$query=$this->db->get("offer_letter");
-		if(!$query->num_rows())
-		{
-			$this->db->insert('offer_letter',$data);
-			if($this->db->affected_rows() > 0)
-			{
-				return true;
-				
-			}
-		}
-		else
-		{
-			$this->db->where('employee_id',$data['employee_id']);
-			$this->db->update('offer_letter',$data);
-			if($this->db->affected_rows() > 0)
-			{
-				return true;
-			}
-		}
->>>>>>> 6a51a23a99260cebddf4e671fde11562b7de8195
 
 		$query = $this->db->get("offer_letter");
 		if (!$query->num_rows()) {
